a3abf59d51575badbd9098b49f4579593a719a72
Federico Terzi <federicoterzi96@gmail.com>
2024-06-14 09:42:12 +0200

    fix flag

--- configure.py
+++ configure.py
@@ -17,7 +17,7 @@ MACOS_ARGS = [
   "--target-os=darwin",
   "--incdir=INCLUDES",
   "--shlibdir=SHARED_LIBS",
-  "--enable-hwaccel=h264_videotoolbox",
+  "--enable-videotoolbox",
 ]
 
 MACOS_X86_64_ARGS = [
